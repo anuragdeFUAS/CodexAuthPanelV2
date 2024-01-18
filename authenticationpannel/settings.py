5eefb2f0c0ccb9630fddebdf2c97e3851b2a109a
anuragdeFUAS <anurag.de@stud.fra-uas.de>
2024-01-18 06:38:04 +0100

    changes to file for deployment

--- authenticationpannel/settings.py
+++ authenticationpannel/settings.py
@@ -29,7 +29,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = "django-insecure-uwz$jar@@8afs+ghw6+zm!9h_by1%^w%o&9&#i-2(4zi_+!b&$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -105,10 +105,7 @@ WSGI_APPLICATION = "authenticationpannel.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(env("DATABASE_URL"))
 }
 
 
